7fac701a371931e5407c8a05a44fa9cea45a84b8
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-11-30 22:01:03 -0300

    Add method get_test_count

--- lua/test/runners/plenary_busted.lua
+++ lua/test/runners/plenary_busted.lua
@@ -14,4 +14,19 @@ function PlenaryBusted:did_run()
   return self.raw_results ~= nil
 end
 
+function PlenaryBusted:parse_results()
+  self.test_count = {
+    total = 0,
+    passed = 0,
+    failed = 0
+  }
+end
+
+function PlenaryBusted:get_test_count()
+  if self.test_count == nil then
+    self:parse_results()
+  end
+  return self.test_count
+end
+
 return PlenaryBusted
